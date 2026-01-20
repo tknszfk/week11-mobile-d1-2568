196b75b8f5d45b6ac56594deef5758944498972a
FlimFli <peckbboy2@gmail.com>
2026-01-20 16:37:44 +0700

    finish listview menu

---
+++ lib/view/listview_menu.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class ListviewMenu extends StatelessWidget {
+  const ListviewMenu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey,
+        title: Text("ListView Menu",style: TextStyle(color: Colors.white),),
+      ),
+      body: Container(
+        child: ListView.separated(itemBuilder: (context, index) {
+          return ListTile(
+            title: Text("MENU-1"),
+            subtitle: Text("SUB-1"),
+            leading: CircleAvatar(child: Icon(Icons.person),),
+            trailing: Icon(Icons.arrow_right),
+            onTap: () {
+              debugPrint("MENU-1");
+            },
+          );
+        }, separatorBuilder: (context, index) {
+          return Divider(color: Colors.black,);
+        }, itemCount: 4),
+      ),
+    );
+  }
+}
