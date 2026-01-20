a4d650a398b8b87d1c123446bc2e20312cf4c6dd
FlimFli <peckbboy2@gmail.com>
2026-01-20 15:42:59 +0700

    finish row widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:week11/view/home.dart';
+import 'package:week11/view/row_page.dart';
 
 void main(){
   runApp(MyApp());
@@ -12,8 +13,7 @@ class MyApp extends StatelessWidget{
   Widget build(BuildContext context){
     return MaterialApp(
       title: "Flutter",
-      home: HomeScreen(
-      ),
+      home: RowPage(),
     );
   }
 }
